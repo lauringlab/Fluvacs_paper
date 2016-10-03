9942937e97a1c4976bd5d3abdc3ff605b9d0f22a
jtmccr1 <mccrone@umich.edu>
2016-10-03 12:55:42 -0400

    get fastq script

---
+++ scripts/get_fastq.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SRA=($(cut -f1 -d '	' $1 |grep -v "sra_accession"))
+    for (( i=0; i<${#SRA[@]}; i++ )); do 
+      echo downloading ${SRA[i]}
+      #prefetch ${SRA[i]}
+      #sra_file=$(ls ~/ncbi/public/sra/${SRA[i]}.sra) 
+      #echo converting $sra_file to fastq
+      #fastq-dump  $sra_file --split-files -O $2
+      #echo removing $sra_file
+      #rm $sra_file
+       done    
