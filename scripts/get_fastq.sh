b4b40266946a9c3261fd2bdb51a17c86cb8f8fc0
jtmccr1 <mccrone@umich.edu>
2016-10-04 10:28:59 -0400

    downloading using wget

--- scripts/get_fastq.sh
+++ scripts/get_fastq.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 SRA=($(cut -f1 -d '	' $1 |grep -v "sra_accession"))
-    for (( i=0; i<${#SRA[@]}; i++ )); do 
-      echo downloading ${SRA[i]}
-      prefetch ${SRA[i]}
-      sra_file=$(ls ~/ncbi/public/sra/${SRA[i]}.sra) 
-      echo converting $sra_file to fastq
-      fastq-dump  $sra_file --split-files -O $2
-      echo removing $sra_file
-      rm $sra_file
-      done    
+for (( i=0; i<${#SRA[@]}; i++ ))
+ do 
+    echo downloading ${SRA[i]}
+     wget -r -np -nd -k -P $2 ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/${SRA[i]:0:6}/${SRA[i]}/${SRA[i]}.sra
+    
+    sra_file=$2${SRA[i]}.sra
+    echo converting $sra_file to fastq
+    fastq-dump  $sra_file --split-files -O $2
+    echo removing $sra_file
+    rm $sra_file
+done    
